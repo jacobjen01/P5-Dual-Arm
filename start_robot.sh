3568de59b3f7b2e5156343c88f43be8849a33c3e
jacobjen01 <jacobjen01@gmail.com>
2025-10-07 13:32:34 +0200

    Figured out how to do p2p movement and made a package for it. There is a homing script now so we can easily set the robot in home

--- start_robot.sh
+++ start_robot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ros2 launch ur_robot_driver ur_control.launch.py ur_type:=ur5e robot_ip:=192.168.1.51 headless_mode:=true
+ros2 launch ur_robot_driver ur_control.launch.py ur_type:=ur5e robot_ip:=192.168.1.100 headless_mode:=true
