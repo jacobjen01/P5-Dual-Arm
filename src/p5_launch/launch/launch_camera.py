f9590ba8284a94a9e5d3e62e595796ae9933574f
Jacob <jacobjen01@gmail.com>
2025-10-16 10:34:45 +0200

    Added launch package with camera launch file

---
+++ src/p5_launch/launch/launch_camera.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        #
+        # Realsense D435i
+        #
+        Node(
+            package='realsense2_camera',
+            namespace='camera',
+            executable='realsense2_camera_node',
+            name='camera'
+        ),
+        #
+        # April tag reader
+        #
+        Node(
+            package='apriltag_ros',
+            namespace='apriltag',
+            executable='apriltag_node',
+            name='apriltag_node',
+            remappings=[
+                ('image_rect', '/camera/camera/color/image_raw'),
+                ('camera_info', '/camera/camera/color/camera_info'),
+            ]
+        ),
+    ])
